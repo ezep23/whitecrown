9eac8677d90902467e77e1770349bb38c7a9ca2f
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-12-02 22:53:09 -0300

    second commit vue

---
+++ whitecrown-vue/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Counter />
+  <TaskList />
+</template>
+
+<script setup>
+  import Counter from './components/Counter.vue';
+  import TaskList from './components/TaskList.vue';
+
+</script>
+
+<style scoped>
+
+</style>
