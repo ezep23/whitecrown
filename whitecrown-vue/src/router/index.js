d487d1f6348683ce65dda30fa20fffded5f132ed
Ezequiel Pistone <ezequielpistoneee@gmail.com>
2024-12-06 21:37:55 -0300

    products maped in Home view

--- whitecrown-vue/src/router/index.js
+++ whitecrown-vue/src/router/index.js
@@ -1,15 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from '../views/Home.vue';
+import ProductDetail from '../views/ProductDetail.vue';
 import Cart from '../views/Cart.vue';
 import Profile from '../views/Profile.vue';
-import Products from '../views/ProductList.vue'
 
 const routes = [
   { path: '/', component: Home },
+  { path: '/product/:id', component: ProductDetail },
   { path: '/cart', component: Cart },
   { path: '/profile', component: Profile },
-  { path: '/products', component: Products },
 ];
 
 const router = createRouter({
--- whitecrown-vue/src/views/Cart.vue
+++ whitecrown-vue/src/views/Cart.vue
@@ -1,7 +1,17 @@
 <template>
-
+    <div>
+        <Header />
+        <CheckoutForm />
+        <Footer />
+    </div>
 </template>
 
-<script>
-
+<script setup>
+    import Header from '../components/Header.vue';
+    import CheckoutForm from '../components/CheckoutForm.vue';
+    import Footer from '../components/Footer.vue';
 </script>
+
+<style scoped>
+
+</style>
